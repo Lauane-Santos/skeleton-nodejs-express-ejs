173b9ae1d35ed4d690f9986de950ca4e9484f9ba
Lauane Santos <86008369+Lauane-Santos@users.noreply.github.com>
2022-03-16 21:01:00 +0000

    Adicionado o comando update (atualizar) ao meu banco de dados

--- models/autor.js
+++ models/autor.js
@@ -1,14 +1,28 @@
 const db = require("../db")
 
 class Autor {
-  static async selecionar(){
+  static async selecionar() {
     const connect = await db.connect();
     return await connect.query("select * from autores");
   }
- static async inserir(data){
+
+  static async inserir(data) {
     const connect = await db.connect();
     const sql = "insert into autores (nome, sobrenome, data_nascimento) values ($1, $2, $3)";
     const values = [data.nome, data.sobrenome, data.data_nascimento]
+    return await connect.query(sql, values);
+  }
+
+   static async atualizar(data, id) {
+    const connect = await db.connect();
+    const sql = "update autores set nome = $1, sobrenome = $2, data_nascimento = $3 where  id = $4)";
+    const values = [data.nome, data.sobrenome, data.data_nascimento, id]
+    return await connect.query(sql, values);
+  }
+  static async deletar(id) {
+    const connect = await db.connect();
+    const sql = "delete from autores where  id = $1)";
+    const values = [id];
     return await connect.query(sql, values);
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const Autor = require ("../models/autor");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const autores = await Autor.selecionar()
-  res.json (autores.rows);
+  res.json(autores.rows);
   /*res.render('index', { title: 'Express' });*/
 });
 
@@ -15,9 +15,20 @@ router.get('/inserir', async function(req, res, next) {
     sobrenome: "Silva",
     data_nascimento: "1999-07-01"
   };
+  
   const autores = await Autor.inserir(autor);
   res.json(autores.rows);
 });
 
+router.get('/atualizar', async function(req, res, next) {
+  let autor = {
+    nome: "Moisés",
+    sobrenome: "Antonio",
+    data_nascimento: "2015-07-01"
+  };
+  
+  const autores = await Autor.atualizar(autor, 3);
+  res.json(autores.rows);
+});
 
 module.exports = router;
