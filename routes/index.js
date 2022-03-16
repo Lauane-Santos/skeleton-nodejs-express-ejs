8a06696fc45c376bd6daf33ae689e0e1c1cbdba9
Lauane Santos <86008369+Lauane-Santos@users.noreply.github.com>
2022-03-16 19:33:14 +0000

    Adicionado o comando de insert (inserir) ao o de dados

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,18 @@ const Autor = require ("../models/autor");
 router.get('/', async function(req, res, next) {
   const autores = await Autor.selecionar()
   res.json (autores.rows);
-  res.render('index', { title: 'Express' });
+  /*res.render('index', { title: 'Express' });*/
 });
+
+router.get('/inserir', async function(req, res, next) {
+  let autor = {
+    nome: "Margarida",
+    sobrenome: "Silva",
+    data_nascimento: "1999-07-01"
+  };
+  const autores = await Autor.inserir(autor);
+  res.json(autores.rows);
+});
+
 
 module.exports = router;
